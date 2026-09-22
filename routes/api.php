252348fbca97d24f364949a82bb68fb0b9302fee
Azkiya Fe Sabella <azkiyafesabella14@gmail.com>
2026-09-22 09:55:30 +0700

    feat: inisialisasi project Laravel dan skema database GARASI

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public Route
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Route (Harus Bawa Token)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
